1a5bb4cf93fb075bf92a3f188d37573154b43a81
Will Lowenkamp <lowenkamp97@gmail.com>
2023-05-14 12:41:33 -0400

    add script.js, connect to index.html

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head></head>
-  <body></body>
-</html>
-<head>
+  <head>
+    <link rel="stylesheet" href="style.css" />
     <title>Will's Website</title>
   </head>
   <body>
     <h1>Hello World!</h1>
     <script src="script.js"></script>
   </body>
-  </html>
+</html>
